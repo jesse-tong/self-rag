5cca9012be65c429beafa295f3a27324808669a2
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2025-03-09 00:35:56 +0700

    Edit for evaluation in Kaggle

--- retrieval_lm/passage_retrieval.py
+++ retrieval_lm/passage_retrieval.py
@@ -193,13 +193,14 @@ class Retriever:
         else:
             print(f"Indexing passages from files {input_paths}")
             start_time_indexing = time.time()
-            self.index_encoded_data(self.index, input_paths, 50000)
+            self.index_encoded_data(self.index, input_paths, 25000)
             print(f"Indexing time: {time.time()-start_time_indexing:.1f} s.")
 
         # load passages
         print("loading passages")
         all_passages = src.data.load_passages(passages)
-        self.passages = all_passages[:150000] # Load only 150000 first passages
+        self.passages = all_passages[:100000] # Load only 150000 first passages
+        all_passages = None
         del all_passages
         self.passage_id_map = {x["id"]: x for x in self.passages}
         print("passages have been loaded")
