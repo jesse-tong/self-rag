3daf23f237b4dfe51a2ed1f0b14b29189aafe627
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2025-03-09 10:28:04 +0700

    Edit for evaluation in Kaggle

--- retrieval_lm/download_demo_corpus.sh
+++ retrieval_lm/download_demo_corpus.sh
@@ -1,5 +1,5 @@
 pip install gdown
-gdown "1IYNAkwawfCDiBL27BlBqGssxFQH9vOux&confirm=t"
+gdown 1IYNAkwawfCDiBL27BlBqGssxFQH9vOux&confirm=t
 mv enwiki_2020_intro_only.zip /opt/kaggle/
 unzip /opt/kaggle/enwiki_2020_intro_only.zip -d "$(pwd)"
 rm /opt/kaggle/enwiki_2020_intro_only.zip
